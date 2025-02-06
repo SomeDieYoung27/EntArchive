0a48da3eff4e2891b1aaddfc471c0a983cb0c6b4
Shashwat Chaturvedi <111681395+SomeDieYoung27@users.noreply.github.com>
2025-02-06 10:59:34 +0530

    feat:Config settings added

---
+++ server/api/config/settings.py
@@ -0,0 +1,29 @@
+from pydantic_settings import BaseSettings
+from typing import List
+import os
+from functools import lru_cache
+
+class Settings(BaseSettings,extra='allow'):
+    ENV : str = 'dev'
+    CHROMADB_TOKEN: str = os.getenv("CHROMADB_TOKEN", "dummy")
+    CHROMADB_SERVER: str = os.getenv("CHROMADB_SERVER", "http://localhost:8080")
+
+    ALLOWED_ORIGINS : List[str] = []
+
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        self.ALLOWED_ORIGINS = (
+            ["http://localhost:3000", "https://www.arxival.xyz"]
+            if self.ENV == "dev"
+            else ["https://www.arxival.xyz", "https://s.arxival.xyz"]
+        )
+
+    class Config:
+        env_file = ".env"
+
+
+    @lru_cache
+    def get_settings():
+        return Settings()
+    
+    settings = get_settings()
